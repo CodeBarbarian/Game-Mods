0dbc415daf767252083a53719e11d82523ac349b
kstrider <kstrider@kstrider.tv>
2016-04-23 22:16:04 +0200

    Cleaned up improper indentation.

--- Factorio/projectx_0.0.1/prototypes/recipes.lua
+++ Factorio/projectx_0.0.1/prototypes/recipes.lua
@@ -1,14 +1,14 @@
 data:extend({
-  {
-    type = "recipe",
-    name = "express-inserter",
-    enabled = false,
-    ingredients =
-    {
-		{"fast-inserter", 1},
-		{"advanced-circuit", 2},
-		{"iron-gear-wheel", 5}
-    },
-    result = "express-inserter"
-  }
+	{
+		type = "recipe",
+		name = "express-inserter",
+		enabled = false,
+		ingredients =
+		{
+			{"fast-inserter", 1},
+			{"advanced-circuit", 2},
+			{"iron-gear-wheel", 5}
+		},
+		result = "express-inserter"
+	}
 })
