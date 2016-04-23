02b57e19fe37bb4fd9d6b7bed04aee3276585ba7
Morten Haugstad <Morten Haugstad>
2016-04-23 18:13:54 +0200

    Initial project commit.

---
+++ Factorio/ProjectX/prototypes/item.lua
@@ -0,0 +1,12 @@
+data:extend({
+  {
+    type = "item",
+    name = "fast-inserter",
+    icon = "__ProjectX__/graphics/icons/express-inserter.png",
+    flags = {"goes-to-quickbar"},
+    subgroup = "inserter",
+    order = "f[express-inserter]",
+    place_result = "express-inserter",
+    stack_size = 50
+  }
+})
